a028da745656055ae442b82aef3906abaafd8203
dcalabrese22 <dcalabrese22@gmail.com>
2017-12-15 21:35:17 -0500

    added Google sign in

--- app/src/main/java/dan/dcalabrese22/com/jobapptracker/MainActivity.java
+++ app/src/main/java/dan/dcalabrese22/com/jobapptracker/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 import dan.dcalabrese22.com.jobapptracker.database.DbOperations;
 import dan.dcalabrese22.com.jobapptracker.interfaces.JobClickHandler;
 import dan.dcalabrese22.com.jobapptracker.objects.Job;
@@ -27,16 +37,30 @@ public class MainActivity extends AppCompatActivity implements JobClickHandler{
     private RecyclerView mRecyclerview;
     private JobListAdapter mAdapter;
     private DbOperations mOperator;
+    private SignInButton mSignInButton;
+    private GoogleSignInClient mGoogleSignInClient;
 
     public static final String JOB_ID_EXTRA = "job_id_extra";
     public static final String JOB_COMPANY_NAME_EXTRA = "company_name_extra";
     public static final String JOB_DATE_APPLIED_EXTRA = "date_applied_extra";
     public static final String JOB_DESCRIPTION_EXTRA = "description_extra";
+    public static final int RC_SIGN_ON = 1;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUi(account);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSignInButton = findViewById(R.id.sign_in_button);
+        mSignInButton.setSize(SignInButton.SIZE_STANDARD);
+        mSignInButton.setOnClickListener(new SignInButtonListener());
+
         mOperator = new DbOperations(this);
         mContext = this;
         mNewJobBtn = findViewById(R.id.btn_new_job);
@@ -48,6 +72,12 @@ public class MainActivity extends AppCompatActivity implements JobClickHandler{
         final LinearLayoutManager manager = new LinearLayoutManager(this);
         mRecyclerview.setLayoutManager(manager);
         mNewJobBtn.setOnClickListener(new NewJobBtnClickListener());
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         ItemTouchHelper.SimpleCallback touchHelperCallback = new ItemTouchHelper.SimpleCallback(0,
                 ItemTouchHelper.LEFT) {
@@ -96,6 +126,18 @@ public class MainActivity extends AppCompatActivity implements JobClickHandler{
         mAdapter.setJobData();
     }
 
+    public void updateUi(GoogleSignInAccount account) {
+        if (account != null) {
+            mRecyclerview.setVisibility(View.VISIBLE);
+            mNewJobBtn.setVisibility(View.VISIBLE);
+            mSignInButton.setVisibility(View.INVISIBLE);
+        } else {
+            mRecyclerview.setVisibility(View.INVISIBLE);
+            mNewJobBtn.setVisibility(View.INVISIBLE);
+            mSignInButton.setVisibility(View.VISIBLE);
+        }
+    }
+
     @Override
     public void onJobClick(Job job) {
         Intent intent = new Intent(mContext, JobDetailActivity.class);
@@ -122,11 +164,37 @@ public class MainActivity extends AppCompatActivity implements JobClickHandler{
     }
 
     private class NewJobBtnClickListener implements View.OnClickListener {
-
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(mContext, NewJobActivity.class);
             startActivity(intent);
+        }
+    }
+
+    private class SignInButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Intent intent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(intent, RC_SIGN_ON);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_ON) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    public void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            updateUi(account);
+        } catch (ApiException e) {
+            e.printStackTrace();
         }
     }
 
@@ -142,8 +210,19 @@ public class MainActivity extends AppCompatActivity implements JobClickHandler{
         int id = item.getItemId();
         switch (id) {
             case (R.id.action_write_to_drive):
+                break;
+            case (R.id.logout):
+                mGoogleSignInClient.signOut()
+                        .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                mRecyclerview.setVisibility(View.INVISIBLE);
+                                mNewJobBtn.setVisibility(View.INVISIBLE);
+                                mSignInButton.setVisibility(View.VISIBLE);
+                            }
+                        });
+        }
 
-            }
         return super.onOptionsItemSelected(item);
     }
 }
